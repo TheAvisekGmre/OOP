3257edabb836cd45b0ded74d71923f2cc11d0c8a
TheAvisekGmre <aghimire074@gmail.com>
2022-03-13 16:19:29 +0545

    binary file exclude test

---
+++ Basic/inputFromUser.cpp
@@ -0,0 +1,12 @@
+// Getting age value from the user and displaying it.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Your age is: " << age << endl;
+    return 0;
+}
